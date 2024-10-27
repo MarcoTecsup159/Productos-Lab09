045abbed7f88a53cf4d0b1018f4ae9add80b8ce5
Marco <marco.rios.c@tecsup.edu.pe>
2024-10-27 16:21:33 -0500

    Agregar el modelo de datos

---
+++ app/src/main/java/com/example/productos/ProductModel.kt
@@ -0,0 +1,11 @@
+package com.example.productos
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductModel(
+    @SerializedName("id") val id: int,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("price") val price: Double,
+    @SerializedName("thumbnail") val thumbnail: String
+)
